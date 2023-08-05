efc81aea92c0e696a0e5d411e6a876712015c7e6
mohsenebrahimyir <mohsenebrahimy.ir@gmail.com>
2023-08-06 01:22:26 +0330

    update

--- R/library.R
+++ R/library.R
@@ -13,6 +13,23 @@ c(
   "tidyverse", 
   "knitr", 
   "bookdown",
-  "readxl"
+  "readxl",
+  "kableExtra"
   ) |>
   recall_packages()
+
+
+## My Table Style --------------------------------------------------------------
+my_table_style <- function(dt) {
+  dt <- dt |> 
+    kable_classic() |>
+    kable_styling(
+      latex_options = "hold_position",
+      full_width = TRUE,
+      html_font = "Roboto",
+      fixed_thead = T,
+      bootstrap_options = c("striped", "hover", "condensed", "responsive")
+    )
+  
+  return(dt)
+}
