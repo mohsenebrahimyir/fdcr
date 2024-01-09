133a19eaf4109ed081806b7b5641be1ef62c5a52
mohsenebrahimyir <mohsenebrahimy.ir@mail.com>
2024-01-09 11:55:05 +0330

    update

--- R/database.R
+++ R/database.R
@@ -1,3 +1,3 @@
 ## READ DATABASE ---------------------------------------------------------------
 xlsx_path <- "./data/live-system-review.xlsx"
-included <- readxl::read_xlsx(xlsx_path, "Included", skip = 1)
+included <- readxl::read_xlsx(xlsx_path, "Included", skip = 0)
